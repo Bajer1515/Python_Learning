de69be13774776795db8075015ecb4e9c42b601a
Piotr Kawa <piotr.w.kawa@gmail.com>
2019-08-04 22:41:18 +0200

    Add comments on code clarity and future work

--- Ex_1.py
+++ Ex_1.py
@@ -14,15 +14,18 @@ print(len(t))
 print(t[1:10])
 
 # Usunięcie pustych linii
+# 1) what is t? text? tribute? variable / method etc. names should be self-explanatory - in the best case you just look at the name and know all of required information. 't' doesn't provide that
+# 2) it does work, but maybe think about list comprehension [line for line in t if line] 
+#     2a) why is there if line? this means that we take only the strings that are True - when are they True? --> check https://stackoverflow.com/a/4531832
 while '' in t:
-    t.remove('')
+    t.remove('')  
 
 print(len(t))
 print(t[1:10])
 
 for i in t:
     t[i].split("  ")
-
+# t = [element.split(' ') for element in t] # list comprehension - once again your solution was ok, but you can do it slightly more elegant way
 
 #t2 = t.split("  ")
 
@@ -32,4 +35,12 @@ print(t2[1:10])
 #t3 = t2.partition('  ')
 
 #print(t3[1:3])
+
+
+### Extraction of the first word. 
+# Look for rsplit() function - as You may have seen words do not contain any spaces in them (even when they contain a dash). This means that you can split by ' '. 
+
+
+
+
 
