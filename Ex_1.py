db1c89bbf21811106a2521948a404ccbafd4a1e5
Bajer1515 <48063326+Bajer1515@users.noreply.github.com>
2019-08-06 00:08:20 +0200

    Add files via upload
    Added list without empty lines and separated words in every line

--- Ex_1.py
+++ Ex_1.py
@@ -11,38 +11,22 @@ dictionary = r.text
 # Rozbicie słownika na osobne linijki
 print('First step - all lines')
 Full_list = dictionary.split("\n")
-print(Full_list[4])
+print(Full_list[0])
 
-# Rozbicie każdej lini na listy pojedynczych słów
-print('Second step - list of lists with words')
-Full_separated_list = [element.split(' ') for element in Full_list]
-print(Full_separated_list[4])
-
-#Creating list without empty positions
+# Change third step with second step.
+#C reating list without empty positions
 print('Third step - no empty lines')
-no_empty = []
-no_empty = [line for line in Full_separated_list if line == True]
-print(no_empty)
+no_empty_list = []
+no_empty_list = [line for line in Full_list if line]
+print(no_empty_list[0])
+
+
+# Creating list of list with separated lines
+print('Second step - list of lists with words')
+Full_separated_list = [element.split(' ') for element in no_empty_list]
+print(Full_separated_list[1])
 
 
 
 
-
-# Usunięcie pustych linii
-#while '' in t:
-#    t.remove('')
-
-#print(len(t))
-#print(t[1:10])
-
-
-
-#t2 = t.split("  ")
-
-#print(len(t2))
-#print(t2[1:10])
-
-#t3 = t2.partition('  ')
-
-#print(t3[1:3])
 
