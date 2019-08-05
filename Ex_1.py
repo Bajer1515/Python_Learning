71c7ed2b94ced815a0483daadb0111cdb7f8bea8
Bajer1515 <48063326+Bajer1515@users.noreply.github.com>
2019-08-05 23:00:29 +0200

    Add files via upload
    Different way to create list of words

--- Ex_1.py
+++ Ex_1.py
@@ -10,37 +10,32 @@ text = r.text
 
 # Rozbicie słownika na osobne linijki
 t = text.split("\n")
-print(len(t))
-print(t[1:10])
+print(t[4])
+# Rozbicie każdej lini na listy pojedynczych słów
+t = [element.split(' ') for element in t]
+print(t[4])
+
+
+
+
+
+
 
 # Usunięcie pustych linii
-# 1) what is t? text? tribute? variable / method etc. names should be self-explanatory - in the best case you just look at the name and know all of required information. 't' doesn't provide that
-# 2) it does work, but maybe think about list comprehension [line for line in t if line] 
-#     2a) why is there if line? this means that we take only the strings that are True - when are they True? --> check https://stackoverflow.com/a/4531832
-while '' in t:
-    t.remove('')  
+#while '' in t:
+#    t.remove('')
 
-print(len(t))
-print(t[1:10])
+#print(len(t))
+#print(t[1:10])
 
-for i in t:
-    t[i].split("  ")
-# t = [element.split(' ') for element in t] # list comprehension - once again your solution was ok, but you can do it slightly more elegant way
+
 
 #t2 = t.split("  ")
 
-print(len(t2))
-print(t2[1:10])
+#print(len(t2))
+#print(t2[1:10])
 
 #t3 = t2.partition('  ')
 
 #print(t3[1:3])
-
-
-### Extraction of the first word. 
-# Look for rsplit() function - as You may have seen words do not contain any spaces in them (even when they contain a dash). This means that you can split by ' '. 
-
-
-
-
 
