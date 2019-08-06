cd9806a6e7123ea5b56b354423bba21563b7bf91
Bajer1515 <48063326+Bajer1515@users.noreply.github.com>
2019-08-06 23:18:10 +0200

    Add files via upload
    Added creating list with only one word from password + creating new file and writing this list

--- Ex_1.py
+++ Ex_1.py
@@ -9,24 +9,36 @@ dictionary = r.text
 #print(text)
 
 # Rozbicie słownika na osobne linijki
+
 print('First step - all lines')
 Full_list = dictionary.split("\n")
 print(Full_list[0])
 
-# Change third step with second step.
-#C reating list without empty positions
-print('Third step - no empty lines')
+
+# C reating list without empty positions
+
+print('Second step - no empty lines')
 no_empty_list = []
 no_empty_list = [line for line in Full_list if line]
 print(no_empty_list[0])
 
 
 # Creating list of list with separated lines
-print('Second step - list of lists with words')
+
+print('Third step - list of lists with words')
 Full_separated_list = [element.split(' ') for element in no_empty_list]
-print(Full_separated_list[1])
+print(Full_separated_list[0])
 
 
+# Creating list with only passwords
+
+print('Fourth step - list with only first word')
+passwords = []
+passwords = [item[0] for item in Full_separated_list]
+print(passwords[0:10])
 
 
-
+# Creating new file to write there passwords
+file = open("Passwords.txt","a+")
+for i in range(len(passwords)):
+    file.write(passwords[i]+"\n")
