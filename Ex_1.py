dcc9a556ed885feb41fda4f4896dc2e3a851de10
Piotr Kawa <piotr.w.kawa@gmail.com>
2019-08-07 20:30:40 +0200

    Update Ex_1.py

--- Ex_1.py
+++ Ex_1.py
@@ -11,22 +11,22 @@ dictionary = r.text
 # Rozbicie słownika na osobne linijki
 
 print('First step - all lines')
-Full_list = dictionary.split("\n")
+Full_list = dictionary.split("\n") # TODO 2): read about Python naming conventions
 print(Full_list[0])
 
 
 # C reating list without empty positions
 
-print('Second step - no empty lines')
+print('Second step - no empty lines') # TODO 4) stick to one convention - use either double quotes or single ones
 no_empty_list = []
-no_empty_list = [line for line in Full_list if line]
+no_empty_list = [line for line in Full_list if line] # TODO 1): you don't have to initialize an empty array if you perform list comprehension
 print(no_empty_list[0])
 
 
 # Creating list of list with separated lines
 
 print('Third step - list of lists with words')
-Full_separated_list = [element.split(' ') for element in no_empty_list]
+Full_separated_list = [element.split(' ') for element in no_empty_list] # TODO 1): here's a proper way of list comprehension
 print(Full_separated_list[0])
 
 
@@ -39,6 +39,7 @@ print(passwords[0:10])
 
 
 # Creating new file to write there passwords
-file = open("Passwords.txt","a+")
-for i in range(len(passwords)):
+file = open("Passwords.txt","a+") # TODO 3): why a+? 
+for i in range(len(passwords)): # TODO 5): can you think of another way of writing this loop?
     file.write(passwords[i]+"\n")
+# TODO 6): read about with open statemenet
