dc8e04fa733051523badce9f7ee59c8aac60e41a
Adam Bajerowicz <adam.bajerowicz@wp.pl>
2019-08-10 21:47:11 +0200

    Add files with words. Random chose of words

--- Ex_2.py
+++ Ex_2.py
@@ -1,5 +1,6 @@
 import argparse
 import random
+import string
 
 # Przy pomocy argparse stworzyć program, który będzie przyjmował dwa argumenty
 # * liczbę słów
@@ -8,10 +9,10 @@ import random
 # Słowa mają zostać pobrane z pliku zapisanego w ramach programu 1.
 
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("number", type = int, help = "Number of randomly chosen words")
-#parser.add_argument("letter", help = "A letter that starts words")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("number", type=int, help="Number of randomly chosen words")
+parser.add_argument("letter", help="A letter that starts words")
+args = parser.parse_args()
 
 # We are opening file as f
 f = open("Passwords.txt", "r+")
@@ -20,6 +21,19 @@ list = f.readlines()
 # Close file
 f.close()
 # Previously there was 'word\n'. We are removing this part '\n'
-words = [w.rstrip('\n') for w in list]
-#print(words[0:100])
+words = [element.rstrip('\n') for element in list if element.rstrip('\n')]
+#print(words)
 
+letters = string.ascii_uppercase
+
+for l in letters:
+    with open('{}_words.txt'.format(l), 'w+') as f:
+        for w in words:
+            if l == w[0]:
+                f.write(w+'\n')
+
+print('Your chosen words:')
+with open('{}_words.txt'.format(args.letter), 'r') as f:
+    list_of_words = f.readlines()
+    for i in range(args.number):
+        print(random.choice(list_of_words))
