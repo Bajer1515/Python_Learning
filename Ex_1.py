cc1d61f5232790c7c5388d3c9021a9939e3289d5
Adam Bajerowicz <adam.bajerowicz@wp.pl>
2019-08-13 22:36:04 +0200

    Add save words in folder dictionaries. Separated task in Ex.

--- Ex_1.py
+++ Ex_1.py
@@ -2,65 +2,61 @@ import requests
 import re
 import string
 
-# Import słownika
+# Import of dictionary
 url = "https://raw.githubusercontent.com/sujithps/Dictionary/master/Oxford%20English%20Dictionary.txt?fbclid=IwAR37WSDQx62jLyMBgGbxKfan8kNkGHQ0eNyaFUunMy8kIWtcbnvoCVmAGEk"
 r = requests.get(url)
 dictionary = r.text
-#print(text)
 
-# Rozbicie słownika na osobne linijki
+
+# Cut the dictionary to separated lines
 
 print('First step - all lines')
 full_list = dictionary.split("\n")
-#print(full_list[0:10])
+
 
 # C reating list without empty positions
 
 print('Second step - no empty lines')
 no_empty_list = [line for line in full_list if line]
-#print(no_empty_list[0:10])
+
 
 # Creating list of list with separated lines
 
 print('Third step - list of lists with words')
 full_separated_list = [element.split(' ') for element in no_empty_list]
-#print(full_separated_list[0:10])
+
 
 # Creating list with only passwords without empty lines
 
 print('Fourth step - list with only first word')
-passwords = [item[0] for item in full_separated_list if item[0].strip('\n')]
-#print(passwords[2016])
+words = [item[0] for item in full_separated_list if item[0].strip('\n')]
 
-# Creating new file to write there passwords
-
-#with open('Passwords.txt','a+') as file:
-#    for i in range(len(passwords)):
-#        file.write(passwords[i]+"\n")
 
 letters = string.ascii_uppercase
 
 # Creating list of indexes
-index = []
+indexes = []
 for l in letters:
-    index.append(passwords.index(f'{l}'))
+    indexes.append(words.index(f'{l}'))
+print(indexes)
 
-# Try to slice list in parts but I have no good idea how to do it
-i = 0
-n = 0
-for l in letters:
-    n = n + 1
-    with open(f'{l}_words', 'w+') as f:
-        if i < index[n]:
-            f.write(passwords[i]+'\n'')
-        elif
+# Creating first and last file
+with open('Dictionaries/A_words.txt', 'w+') as fA:
+    A_words = words[indexes[0]:indexes[1]]
+    for word in A_words:
+        fA.write(word+'\n')
 
+with open('Dictionaries/Z_words.txt', 'w+') as fZ:
+    Z_words = words[indexes[-1]:len(words)]
+    for word in Z_words:
+        fZ.write(word+'\n')
 
-#for l in letters:
-#    with open(f'{l}_words.txt', 'w+'):
-#        for passwords.index(f'{l}') in range()
+let = letters[1:-1]
 
-
-
-
-
+i = 1
+for l in let:
+    new_words = words[indexes[i]:indexes[i+1]]
+    i = i + 1
+    with open(f'Dictionaries/{l}_words.txt', 'w+') as f:
+        for word in new_words:
+            f.write(word+'\n')
--- Ex_2.py
+++ Ex_2.py
@@ -14,29 +14,8 @@ parser.add_argument("number", type=int, help="Number of randomly chosen words")
 parser.add_argument("letter", help="A letter that starts words")
 args = parser.parse_args()
 
-# We are opening file as f
-#f = open("Passwords.txt", "r+")
-# Save data from file into list
-#list = f.readlines()
-# Close file
-
-#f.close()
-
-# Previously there was 'word\n'. We are removing this part '\n'
-
-#words = [element.rstrip('\n') for element in list if element.rstrip('\n')]
-#print(words)
-
-#letters = string.ascii_uppercase
-
-#for l in letters:
-#    with open(f'{l}_words.txt', 'w+') as f:
-#        for w in words:
-#            if l == w[0]:
-#                f.write(w+'\n')
-
 print('Your chosen words:')
-with open(f'{args.letter}_words.txt', 'r') as f:
+with open(f'Dictionaries/{args.letter}_words.txt', 'r') as f:
     list_of_words = f.readlines()
     for i in range(args.number):
         print(random.choice(list_of_words))
