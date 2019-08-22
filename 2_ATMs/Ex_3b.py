c16fd393e47775f071625f1e83e8924eeba9abf0
Adam Bajerowicz <adam.bajerowicz@wp.pl>
2019-08-22 23:50:52 +0200

    Another try

--- 2_ATMs/Ex_3b.py
+++ 2_ATMs/Ex_3b.py
@@ -3,20 +3,53 @@ import json
 #class Operations:
     #def __init__(self):
 
-    #def authorisation():
-     #   name = str(input('Your name: '))
-      #  PIN = str(input('Your PIN: '))
+#To tak na próbę zanim nauczę się wczytywać z pliku i do niego zapisywać od razu
+data = {
+    'ATMs':[
+        {'name': 'ATM_1', 'balance': 10000},
+        {'name': 'ATM_2', 'balance': 20000},
+        {'name': 'ATM_3', 'balance': 30000},
+        {'name': 'ATM_4', 'balance': 40000},
+        {'name': 'ATM_5', 'balance': 50000}
+    ]
+}
+users = {
+    'Clients':[
+        {'name': 'Adam Bajerowicz', 'PIN': '0001', 'balance': 15000},
+        {'name': 'Lukasz Klekowski', 'PIN': '0002', 'balance': 20000},
+        {'name': 'Piotr Kawa', 'PIN': '0003', 'balance': 25000}
+    ]
+}
+x = users['Clients']
+print(x)
 
-    #def payment():
+def choice():
+    print('You can use ATM no. 1, 2, 3, 4 or 5. Which one do You like?')
+    n = int(input('I\'d like to choose ATM number: '))
+    if (n < 1 & n > 5):
+        n = int(input('Wrong! Select correct ATM!'))
 
-    #def withdrawal():
 
-    #def balance():
+def authorisation():
+    print('****   Login   ****')
+    name = str(input('Your name: '))
+    PIN = str(input('Your PIN: '))
 
-with open('Clients', 'r') as file:
-    loaded_json = json.loads(file)
 
-print(loaded_json)
+#    def payment():
+
+#    def withdrawal():
+
+#    def balance():
+
+#with open('Clients', 'r') as file:
+#    loaded_json = json.loads(file)
+
+#print(loaded_json)
+
+choice()
+authorisation()
+
 
 
 #1. Wczytanie listy bankomatów.
